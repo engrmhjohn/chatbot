4b0729fd4e73d6e0c5856a1990a96d0ec14b6fb1
Mehedi <johnsubcse@gmail.com>
2022-12-01 19:29:07 +0600

    chatbot first commint

---
+++ app/Http/Controllers/BotmanController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+  
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use BotMan\BotMan\Messages\Incoming\Answer;
+  
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function botmanHandle()
+    {
+        $botman = app('botman');
+  
+        $botman->hears('{message}', function($botman, $message) {
+  
+            if ($message == 'hi') {
+                $this->askName($botman);
+            }else{
+                $this->askName($botman);
+            }
+  
+        });
+  
+        $botman->listen();
+    }
+  
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askName($botman)
+    {
+        $botman->ask('Hello! What is your Name?', function(Answer $answer) {
+  
+            $name = $answer->getText();
+  
+            $this->say('Nice to meet you '.$name);
+        });
+    }
+}
